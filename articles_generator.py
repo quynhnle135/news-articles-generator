e2717c8bbc322b34ccc13aa1b8b51ac6d846d6d1
Quinn Le <quynhnle135@gmail.com>
2023-12-17 21:05:10 -0500

    Updated get_this_week_articles function

--- articles_generator.py
+++ articles_generator.py
@@ -64,23 +64,43 @@ def get_latest_articles(args):
 
 
 def get_this_week_articles(args):
-    base_url = f"https://newsapi.org/v2/everything?qInTitle=OpenAI&from={formatted_start_date}&to={formatted_end_date}&sortBy=popularity&language=en"
     today = date.today()
     start_date = today - timedelta(days=today.weekday())
     end_date = start_date + timedelta(days=6)
     formatted_start_date = date.strftime(start_date, "%Y-%m-%d")
     formatted_end_date = date.strftime(end_date, "%Y-%m-%d")
-    url = f"https://newsapi.org/v2/everything?qInTitle=OpenAI&from={formatted_start_date}&to={formatted_end_date}&sortBy=popularity&language=en"
-    response = requests.request(method="GET", url=url, headers=header).json()
-    articles = response["articles"][:20]
-    count = 1
-    for item in articles:
-        print(f"{count}. {item['title']}\n"
-              f"Written by: {item['author']}\n"
-              f"Description: {item['description']}\n"
-              f"Read the full article at: {item['url']}\n")
-        count += 1
+    base_url = f"https://newsapi.org/v2/everything"
+    params = {
+        "from": formatted_start_date,
+        "to": formatted_end_date,
+        "q": "technology",
+        "sortBy": "popularity",
+        "language": "en"
+    }
 
+    # Adding provided arguments to the parameters
+    if args.keyword:
+        params["qInTitle"] = args.keyword
+    if args.sortby:
+        params["sortBy"] = args.sortby
+    if args.domain:
+        params["domains"] = args.domain
+    if args.category:
+        params["category"] = args.category
+    if args.language:
+        params["language"] = args.language
+
+    try:
+        response = requests.request(method="GET", url=base_url, headers=header, params=params).json()
+        articles = response["articles"][:10]
+        for count, article in enumerate(articles, start=1):
+            print(f"{count}. {article['title']}\n"
+                  f"Written by: {article['author']}\n"
+                  f"Description: {article['description']}\n"
+                  f"Read the full article at: {article['url']}\n")
+        return response
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
 
 
 
--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
                         help="Retrieve a list of top news publishers. Can be filtered by category, country, and language.")
     parser.add_argument("-a", "--articles", action="store_true",
                         help="Fetch the latest news articles. Filters like keyword, sortby, domain, category, country, and language can be applied.")
+    parser.add_argument("-w", "--weekly", action="store_true", help="Fetch the latest news articles of this week. Filters like keyword, sortby, domain, category, country, and language can be applied.")
+
+    # Optional arguments
     parser.add_argument("-ca", "--category", type=str,
                         help="Filter publishers/articles by a specific news category (e.g., 'business', 'sports'). Applies to both publishers and articles.")
     parser.add_argument("-co", "--country", type=str,
@@ -15,7 +18,7 @@ def main():
     parser.add_argument("-la", "--language", type=str,
                         help="Filter publishers/articles by language using ISO language codes (e.g., 'en' for English, 'es' for Spanish). Applies to both publishers and articles.")
     parser.add_argument("-k", "--keyword", type=str, help="Search articles with a specific keyword in the title.")
-    parser.add_argument("-d", "--domain", type=str, help="Search articles in a specific domain.")
+    parser.add_argument("-do", "--domain", type=str, help="Search articles in a specific domain.")
     parser.add_argument("-so", "--sortby", type=str, choices=['publishedAt', 'relevancy', 'popularity'],
                         help="Sort retrieved articles by 'publishedAt', 'relevancy', or 'popularity'. Applicable only for articles.")
 
@@ -28,6 +31,10 @@ def main():
     if args.articles:
         print("---10 Latest Articles---")
         articles_generator.get_latest_articles(args)
+
+    if args.weekly:
+        print("---10 Articles of this week---")
+        articles_generator.get_this_week_articles(args)
 
 
 if __name__ == "__main__":
